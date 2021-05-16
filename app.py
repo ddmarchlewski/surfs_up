8d81d2d2fa2ab76b41714a23242b03f5fe8efe87
ddmarchlewski <80054925+ddmarchlewski@users.noreply.github.com>
2021-05-16 15:16:40 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#import dependencies
 import datetime as dt
 import numpy as np
 import pandas as pd
@@ -8,18 +7,13 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
-#create engine, reflect database
-
 engine = create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-#create session link
 session = Session(engine)
-
-#define flask app--define welcome route
 
 app = Flask(__name__)
 @app.route("/")
@@ -41,6 +35,12 @@ def precipitation():
     filter(Measurement.date >= prev_year).all()
    precip = {date: prcp for date, prcp in precipitation}
    return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
 
 if __name__ == "__main__":
     app.run()
