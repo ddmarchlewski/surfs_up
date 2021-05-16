27b0c7249d44320315589379155cd236b9b093c2
ddmarchlewski <80054925+ddmarchlewski@users.noreply.github.com>
2021-05-16 14:11:45 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+#import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+#create engine, reflect database
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#create session link
+session = Session(engine)
+
+#define flask app--define welcome route
 
 app = Flask(__name__)
-
-@app.route('/')
-
-def hello_world():
-    return 'Hello world'
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+@app.route("/api/v1.0/precipitation")
 
 if __name__ == "__main__":
     app.run()
